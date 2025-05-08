85e5c897c161732a3b4c78b9bb648abd23e6fbf0
MrHyaena <dolezalmartin131@gmail.com>
2025-05-08 13:54:23 +0200

    Solved calendar problem, active days over years
    - I am basically comparing current datey with
    range that the iser chose

--- app/app/_components/Datepickers/DatepickerToggle.tsx
+++ app/app/_components/Datepickers/DatepickerToggle.tsx
@@ -20,18 +20,32 @@ export default function DatepickerToggle({ setToggle }: Props) {
   const currentDate = new Date();
   const [currentMonth, setCurrentMonth] = useState(currentDate.getMonth());
   const [currentYear, setCurrentYear] = useState(currentDate.getFullYear());
-  const [daysCurrentMonth, setDaysCurrentMonth] = useState<number[]>([]);
+  const [daysCurrentMonth, setDaysCurrentMonth] = useState<Date[]>([]);
   const [daysLastMonth, setDaysLastMonth] = useState<number[]>([]);
+
   const [fieldPick, setFieldPick] = useState(1);
   const [fieldColors, setFieldColors] = useState(["white", "#f4f4f5"]);
   const [firstDate, setFirstDate] = useState(new Date());
-  const [secondDate, setSecondDate] = useState(new Date());
+  const [secondDate, setSecondDate] = useState<any>(new Date());
   const [clientFirstDate, setClientFirstDate] = useState("-");
   const [clientSecondDate, setClientSecondDate] = useState("-");
-
   const { setDaterange } = useContext(DaterangeContext);
 
-  const [dayRange, setDayRange] = useState<number[]>([]);
+  const [dayRange, setDayRange] = useState<{
+    firstYear: any;
+    firstMonth: any;
+    firstDay: any;
+    secondYear: any;
+    secondMonth: any;
+    secondDay: any;
+  }>({
+    firstYear: null,
+    firstMonth: null,
+    firstDay: null,
+    secondYear: null,
+    secondMonth: null,
+    secondDay: null,
+  });
 
   const daysOfWeek = ["Po", "Út", "St", "Čt", "Pá", "So", "Ne"];
   const monthsOfYear = [
@@ -56,65 +70,161 @@ export default function DatepickerToggle({ setToggle }: Props) {
 
   //first populate
   useEffect(() => {
+    //Number of days in current month
     const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
-    const daysInLastMonth = new Date(
-      currentYear,
-      currentMonth + 1,
-      0
-    ).getDate();
-    const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
-    const daysLeftFromLastMonth = daysInLastMonth - firstDayOfMonth;
-    const daysArray: number[] = [];
+
+    //Last weeekday of last month
+    let lastDayOfLastMonth = new Date(currentYear, currentMonth, 0).getDay();
+    if (lastDayOfLastMonth == 0) {
+      lastDayOfLastMonth = 7;
+    }
+    const daysArray: Date[] = [];
     const lastDaysArray: number[] = [];
 
-    for (let n = daysLeftFromLastMonth + 1; n < daysInLastMonth; n++) {
-      lastDaysArray.push(n);
+    for (let i = 1; i < daysInMonth + 1; i++) {
+      daysArray.push(new Date(currentYear, currentMonth, i));
+      console.log(new Date(currentYear, currentMonth, i));
     }
 
-    let i = 1;
-    while (i < daysInMonth + 1) {
-      daysArray.push(i);
-      i++;
+    for (let n = 1; n < lastDayOfLastMonth + 1; n++) {
+      lastDaysArray.push(n);
     }
 
     setDaysLastMonth(lastDaysArray);
     setDaysCurrentMonth(daysArray);
   }, []);
 
+  //setting days in month on month change
   useEffect(() => {
+    //Number of days in current month
     const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
-    const daysInLastMonth = new Date(
-      currentYear,
-      currentMonth + 1,
-      0
-    ).getDate();
-    const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
-    const daysLeftFromLastMonth = daysInLastMonth - firstDayOfMonth;
-    const daysArray: number[] = [];
+
+    //Last weeekday of last month
+    let lastDayOfLastMonth = new Date(currentYear, currentMonth, 0).getDay();
+    if (lastDayOfLastMonth == 0) {
+      lastDayOfLastMonth = 7;
+    }
+    const daysArray: Date[] = [];
     const lastDaysArray: number[] = [];
 
-    for (let n = daysLeftFromLastMonth + 1; n < daysInLastMonth; n++) {
+    for (let i = 1; i < daysInMonth + 1; i++) {
+      daysArray.push(new Date(currentYear, currentMonth, i));
+      console.log(new Date(currentYear, currentMonth, i));
+    }
+
+    for (let n = 1; n < lastDayOfLastMonth + 1; n++) {
       lastDaysArray.push(n);
     }
-
-    let i = 1;
-    while (i < daysInMonth + 1) {
-      daysArray.push(i);
-      i++;
-    }
-
-    console.log(daysArray);
-    console.log(lastDaysArray);
 
     setDaysLastMonth(lastDaysArray);
     setDaysCurrentMonth(daysArray);
   }, [currentMonth]);
 
+  //Pick a date when clicking on date
+  function pickDate(index: any) {
+    const newDate = new Date(currentYear, currentMonth, index + 1);
+    const clientDate =
+      index + 1 + "." + (newDate.getMonth() + 1) + "." + newDate.getFullYear();
+    console.log(index);
+    //First date rules
+    if (fieldPick == 1) {
+      setFirstDate(newDate);
+      setSecondDate(null);
+
+      setClientFirstDate(clientDate);
+      setClientSecondDate("-");
+
+      setFieldPick(2);
+      setDayRange({
+        firstYear: newDate.getFullYear(),
+        firstMonth: newDate.getMonth(),
+        firstDay: index,
+        secondYear: null,
+        secondMonth: null,
+        secondDay: null,
+      });
+    }
+
+    //Second date rules
+    if (fieldPick == 2) {
+      if (clientFirstDate == "-") {
+        setFieldPick(1);
+      }
+
+      if (clientFirstDate !== "-") {
+        if (newDate > firstDate) {
+          setSecondDate(newDate);
+          setClientSecondDate(clientDate);
+          setFieldPick(1);
+          setDayRange({
+            ...dayRange,
+            secondYear: newDate.getFullYear(),
+            secondMonth: newDate.getMonth(),
+            secondDay: index,
+          });
+        } else {
+          setClientFirstDate(clientDate);
+          setFieldPick(2);
+          setDayRange({
+            firstYear: newDate.getFullYear(),
+            firstMonth: newDate.getMonth(),
+            firstDay: index,
+            secondYear: null,
+            secondMonth: null,
+            secondDay: null,
+          });
+        }
+      }
+    }
+  }
+
+  function changeMonth(direction: string) {
+    if (direction == "plus") {
+      if (currentMonth == 11) {
+        setCurrentYear(currentYear + 1);
+        setCurrentMonth(0);
+      } else {
+        setCurrentMonth(currentMonth + 1);
+      }
+    }
+
+    if (direction == "minus") {
+      if (currentMonth == 0) {
+        setCurrentYear(currentYear - 1);
+
+        setCurrentMonth(11);
+      } else {
+        setCurrentMonth(currentMonth - 1);
+      }
+    }
+  }
+
+  function dateIsActive(date: Date) {
+    const day = date.getDate() - 1;
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    const stringDate = day + 1 + "." + month + "." + year;
+
+    const dayTwo = firstDate.getDate();
+    const monthTwo = firstDate.getMonth();
+    const yearTwo = firstDate.getFullYear();
+
+    const dateOne = new Date(year, month - 1, day + 1);
+    const dateTwo = new Date(yearTwo, monthTwo, dayTwo);
+
+    if (dateOne >= firstDate && dateOne <= secondDate) {
+      return true;
+    }
+    if (clientFirstDate == stringDate) {
+      return true;
+    }
+  }
+
   return (
     <>
-      <div className="w-screen h-screen fixed top-0 left-0 flex items-center justify-center bg-zinc-700/80">
+      <div className="w-screen h-screen fixed top-0 left-0 flex items-start pt-50 justify-center bg-zinc-700/80">
         <div>
-          <div className="bg-primary text-textLight p-5 py-6 rounded-t-md justify-between items-center hidden lg:flex">
+          <div className="bg-primary text-textLight p-5 py-6 rounded-t-md justify-between items-center flex">
             <h5>Zvolte datum doručení a odvozu</h5>
             <FontAwesomeIcon
               icon={faXmark}
@@ -154,18 +264,20 @@ export default function DatepickerToggle({ setToggle }: Props) {
               </div>
               <div className="flex justify-between items-center py-5">
                 <FontAwesomeIcon
+                  className="cursor-pointer"
                   icon={faChevronLeft}
                   onClick={() => {
-                    setCurrentMonth(currentMonth - 1);
+                    changeMonth("minus");
                   }}
                 />
                 <p className="font-semibold">
                   {monthsOfYear[currentMonth]} {currentYear}
                 </p>
                 <FontAwesomeIcon
+                  className="cursor-pointer"
                   icon={faChevronRight}
                   onClick={() => {
-                    setCurrentMonth(currentMonth + 1);
+                    changeMonth("plus");
                   }}
                 />
               </div>
@@ -189,103 +301,23 @@ export default function DatepickerToggle({ setToggle }: Props) {
                   );
                 })}
                 {daysCurrentMonth.map((day, index) => {
+                  let getDay = day.getDate();
+
                   return (
                     <>
                       <div
                         onClick={() => {
-                          if (fieldPick == 1) {
-                            if (index < dayRange[1]) {
-                              const newDate = new Date(
-                                currentYear,
-                                currentMonth,
-                                index + 1
-                              );
-                              console.log(dayRange);
-                              setDayRange([index, dayRange[1]]);
-                              setClientFirstDate(
-                                index +
-                                  1 +
-                                  "." +
-                                  currentMonth +
-                                  "." +
-                                  currentYear
-                              );
-                              setFirstDate(newDate);
-                              setFieldPick(2);
-                            }
-                            if (index > dayRange[1]) {
-                              const newDate = new Date(
-                                currentYear,
-                                currentMonth,
-                                index + 1
-                              );
-                              console.log(dayRange);
-
-                              setDayRange([dayRange[0], index]);
-
-                              setClientSecondDate(
-                                index +
-                                  1 +
-                                  "." +
-                                  currentMonth +
-                                  "." +
-                                  currentYear
-                              );
-                              setSecondDate(newDate);
-                              setFieldPick(1);
-                            }
-                          }
-                          if (fieldPick == 2) {
-                            if (index > dayRange[0]) {
-                              const newDate = new Date(
-                                currentYear,
-                                currentMonth,
-                                index + 1
-                              );
-                              console.log(dayRange);
-
-                              setDayRange([dayRange[0], index]);
-
-                              setClientSecondDate(
-                                index +
-                                  1 +
-                                  "." +
-                                  currentMonth +
-                                  "." +
-                                  currentYear
-                              );
-                              setSecondDate(newDate);
-                              setFieldPick(1);
-                            }
-                            if (index < dayRange[0]) {
-                              const newDate = new Date(
-                                currentYear,
-                                currentMonth,
-                                index + 1
-                              );
-                              console.log(dayRange);
-                              setDayRange([index, dayRange[1]]);
-                              setClientFirstDate(
-                                index +
-                                  1 +
-                                  "." +
-                                  currentMonth +
-                                  "." +
-                                  currentYear
-                              );
-                              setFirstDate(newDate);
-                              setFieldPick(2);
-                            }
-                          }
+                          console.log(index);
+                          pickDate(index);
                         }}
                       >
-                        {index >= dayRange[0] && index <= dayRange[1] ? (
+                        {dateIsActive(day) ? (
                           <p className="bg-primary/40 h-10 flex items-center justify-center hover:bg-primary/40 rounded-md cursor-pointer">
-                            {day}
+                            {getDay}
                           </p>
                         ) : (
                           <p className="bg-zinc-100 h-10 flex items-center justify-center hover:bg-primary/40 rounded-md cursor-pointer">
-                            {day}
+                            {getDay}
                           </p>
                         )}
                       </div>
