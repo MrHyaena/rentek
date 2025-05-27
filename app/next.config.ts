0c83f7f4ed66136891ef831f8162ee0e08857cbe
MrHyaena <dolezalmartin131@gmail.com>
2025-05-27 19:33:02 +0200

    Fixing nextjs image bug

--- app/next.config.ts
+++ app/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [new URL("http://localhost:1337/**")],
+    remotePatterns: [new URL(`${process.env.STRAPI}/**`)],
   },
   env: {
     STRAPI: process.env.STRAPI,
