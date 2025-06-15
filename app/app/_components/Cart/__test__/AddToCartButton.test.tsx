7e91e09d9daa760dbbc75eb8f7deb3a1edc4467f
MrHyaena <dolezalmartin131@gmail.com>
2025-06-15 21:58:42 +0200

    AddToCartButton test
    - Tests for realAmount zero and non zero
    - Test for correct rendering of rental item

--- app/app/_components/Cart/__test__/AddToCartButton.test.tsx
+++ app/app/_components/Cart/__test__/AddToCartButton.test.tsx
@@ -3,6 +3,8 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/vitest";
 import "@testing-library/react";
 import AddToCartButton from "../_components/AddToCartButton";
+import { CartContext } from "@/app/_context/CartContext";
+import { DaterangeContext } from "@/app/_context/DaterangeContext";
 
 describe("AddToCartButton", () => {
   //Mock data
@@ -16,6 +18,20 @@ describe("AddToCartButton", () => {
     ],
     item: { documentId: "4f48ew45fre9w", amount: 5, pricingType: "rental" },
   };
+
+  const cart: any = [{ item: { documentId: "4f48ew45fre9w" }, count: 3 }];
+  function setCart() {}
+  const daterange = {
+    startDate: new Date(2025, 6, 14, 10),
+    endDate: new Date(2025, 6, 15, 10),
+    startIsValid: true,
+    endIsValid: true,
+  };
+  function setDaterange() {}
+  const saleIndex = 1;
+  const numberOfDays = 1;
+  function setNumberOfDays() {}
+
   const dataRealAmountZero = {
     timeslotsTwo: [
       {
@@ -29,7 +45,19 @@ describe("AddToCartButton", () => {
 
   const { timeslots, item } = dataRealAmountNotZero;
   const { rerender } = render(
-    <AddToCartButton item={item} timeslots={timeslots} />
+    <DaterangeContext.Provider
+      value={{
+        daterange,
+        setDaterange,
+        saleIndex,
+        numberOfDays,
+        setNumberOfDays,
+      }}
+    >
+      <CartContext.Provider value={{ cart, setCart }}>
+        <AddToCartButton item={item} timeslots={timeslots} />
+      </CartContext.Provider>
+    </DaterangeContext.Provider>
   );
 
   //Tests
@@ -40,11 +68,32 @@ describe("AddToCartButton", () => {
 
   it("Amount of cartItem rendered correctly", () => {
     const cartItemAmountParagraph = screen.getByTestId("cartItemAmount");
-    expect(cartItemAmountParagraph).toHaveTextContent("2");
+    expect(cartItemAmountParagraph).toHaveTextContent("3");
   });
 
   it("Correct button shown when real amount more than zero", () => {
     const buttonElement = screen.getByTestId("realAmountNotZero");
+    expect(buttonElement).toBeTruthy();
+  });
+
+  it("Correct button shown when real amount more than zero", () => {
+    const { timeslotsTwo, itemTwo } = dataRealAmountZero;
+    rerender(
+      <DaterangeContext.Provider
+        value={{
+          daterange,
+          setDaterange,
+          saleIndex,
+          numberOfDays,
+          setNumberOfDays,
+        }}
+      >
+        <CartContext.Provider value={{ cart, setCart }}>
+          <AddToCartButton item={itemTwo} timeslots={timeslotsTwo} />
+        </CartContext.Provider>
+      </DaterangeContext.Provider>
+    );
+    const buttonElement = screen.getByTestId("realAmountZero");
     expect(buttonElement).toBeTruthy();
   });
 });
